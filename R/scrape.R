d0337e18f884d349ea94a441423679dc65bcb083
relund <junk@relund.dk>
2020-01-27 11:27:53 +0100

    test

---
+++ R/scrape.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @return NULL
+#' @export
+#'
+#' @examples 
+#' scrape()
+scrape <- function() {
+  dat <- tibble::as_tibble(cars)
+  readr::write_csv2(dat, "./data/data.csv", append = T)
+  invisible(NULL)
+}
